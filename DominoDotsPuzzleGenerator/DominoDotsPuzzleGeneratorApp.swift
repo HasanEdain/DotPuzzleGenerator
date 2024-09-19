429013d9d2204fef39c2f6c9b3791589c7795c7a
Hasan Edain <hasane@npcunlimited.com>
2024-09-18 22:10:38 -0700

    Initial Commit

---
+++ DominoDotsPuzzleGenerator/DominoDotsPuzzleGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DominoDotsPuzzleGeneratorApp.swift
+//  DominoDotsPuzzleGenerator
+//
+//  Created by Hasan Edain on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DominoDotsPuzzleGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DominoDotsPuzzleGeneratorTests/DominoDotsPuzzleGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DominoDotsPuzzleGeneratorTests.swift
+//  DominoDotsPuzzleGeneratorTests
+//
+//  Created by Hasan Edain on 9/18/24.
+//
+
+import Testing
+@testable import DominoDotsPuzzleGenerator
+
+struct DominoDotsPuzzleGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
